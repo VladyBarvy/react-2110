a4621d820d389cbf3faaa165f80bfdb371dc0896
VladyBarvy <vladbarskoff@yandex.ru>
2024-11-06 21:59:22 +0300

    061124 add night theme

--- react-edu-2110/src/App.jsx
+++ react-edu-2110/src/App.jsx
@@ -17,10 +17,21 @@ import SliderDog from './SliderDog.jsx';
 
 
 
+// Компонент для переключения темы
+const ThemeToggle = ({ onToggle }) => {
+  return (
+    <button onClick={onToggle} className="theme-toggle-btn">
+      Переключить тему
+    </button>
+  );
+};
+
+
 function App() {
 
 
-
+  // Хранение текущей темы (по умолчанию - светлая)
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const [todos, setTodos] = useState(() => {
     const storedTodos = localStorage.getItem('todos');
@@ -70,6 +81,12 @@ function App() {
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
     localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setIsDarkMode(savedTheme === 'dark');
+    }
+
   }, [todos, completedTasks]);
 
 
@@ -77,194 +94,54 @@ function App() {
 
 
 
+  // Эффект для применения темы
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDarkMode]);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // const [todos, setTodos] = useState([]); было
-/*
-  const [todos, setTodos] = useState(() => {
-    // Загружаем задачи из Local Storage при инициализации
-    const storedTodos = localStorage.getItem('todos');
-    return storedTodos ? JSON.parse(storedTodos) : []; // Возвращаем массив задач или пустой массив
-  });
-*/
-
-
-
-
-
-  //const addTodo = (todo) => setTodos([...todos, todo]); // no
-
-
-
-
-
-  // const removeTodo = (index) => {
-  //   const newTodos = todos.filter((_, i) => i !== index);
-  //   setTodos(newTodos);
-  // };
-/*
-  const removeTodo = (index) => {
-    setTodos((prevTodos) => {
-      const updatedTodos = prevTodos.filter((_, i) => i !== index);
-      localStorage.setItem('todos', JSON.stringify(updatedTodos)); // Обновляем Local Storage
-      return updatedTodos;
-    });
+  // Функция для переключения темы
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
   };
-*/
-
-
-
-
-
-
-
-
-
-
-
-  // const updateTodo = (index, newTitle) => {
-  //   setTodos((prevTodos) => {
-  //     const updatedTodos = [...prevTodos];
-  //     updatedTodos[index] = { ...updatedTodos[index], title: newTitle }; // Обновляем название
-  //     return updatedTodos;
-  //   });
-  // };
-
-/*
-  const updateTodo = (index, newTitle) => {
-    setTodos((prevTodos) => {
-      const updatedTodos = [...prevTodos];
-      updatedTodos[index] = { ...updatedTodos[index], title: newTitle }; // Обновляем название
-      localStorage.setItem('todos', JSON.stringify(updatedTodos)); // Обновляем Local Storage
-      return updatedTodos;
-    });
-  };
-*/
-
-
-
-
-
-
-
-    // Сохраняем todos в Local Storage при каждом изменении
-    /*
-    useEffect(() => {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos]);
-*/
-
-
-
-
-
-
 
 
   return (
     <>
-      {/* <div>
-        <ButtonOne />
-      </div>
-
-      <div>
-        <Toolbar />
-      </div> */}
-
-
-      {/* <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<CurrencyConverter />} />
-          <Route path="/rates" element={<ExchangeRates />} />
-        </Routes>
-      </Router> */}
-
-
-      {/* <div>
-        <SliderDog />
-      </div> */}
-
-            {/* <div>
-        <ToDoNext />
-      </div> */}
-
-
-
-
-
-
       <div className="global-card">
         <div className="head_title_name">
-          <h1 class="change-text">Task lisT</h1>
+          <h1 class="change-text" onClick={toggleTheme}>Task lisT</h1>
         </div>
-        {/* <Form_todolist addTodo={addTodo} /> */}
         <Form_todolist todos={todos} setTodos={setTodos} />
         <div>
-          {/* <List_todolist todos={todos} removeTodo={removeTodo} /> */}
           <List_todolist todos={todos} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
       </div>
-
-
-
-
-
-
-
-
-
-
-
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
@@ -280,4 +157,39 @@ export default App
 <div class="right-column">
   <button type="button" class="button">Кнопка</button>
 </div>
+*/
+
+
+
+
+
+
+
+
+/*
+       <div>
+        <ButtonOne />
+      </div>
+
+      <div>
+        <Toolbar />
+      </div> 
+
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<CurrencyConverter />} />
+          <Route path="/rates" element={<ExchangeRates />} />
+        </Routes>
+      </Router> 
+
+
+      <div>
+        <SliderDog />
+      </div> 
+
+            <div>
+        <ToDoNext />
+      </div> 
 */
